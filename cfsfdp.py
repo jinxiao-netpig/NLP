3f66e4aae7e5f0bc03cf61983a3fc41bf141ba58
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-03-01 15:23:26 +0800

    Create cfsfdp.py

---
+++ cfsfdp.py
@@ -0,0 +1,26 @@
+class CFSFDP:
+    def __init__(self, epsilon, threshold):
+        self.epsilon = epsilon  # 距离阈值
+        self.threshold = threshold  # 密度峰值阈值
+        pass
+
+    def fit(self):
+        pass
+
+    def build_local_density_list(self):
+        pass
+
+    def build_relative_density_list(self):
+        pass
+
+    def get_distance(self):
+        pass
+
+    def compute_distance(self):
+        pass
+
+    def build_distance_matrix(self):
+        pass
+
+    def build_density_peaks_list(self):
+        pass
