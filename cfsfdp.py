0014337ef99acf14d697e7ad78756845a8130ab3
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-03-01 16:10:10 +0800

    add the method of computing euclidean distance

--- cfsfdp.py
+++ cfsfdp.py
@@ -1,6 +1,13 @@
+import numpy as np
+
+
 class CFSFDP:
+    pattern = [
+        "euclidean_distance",
+    ]
+
     def __init__(self, epsilon, threshold):
-        self.epsilon = epsilon  # 距离阈值
+        self.epsilon = epsilon  # 距离阈值，用于确定某点的局部密度
         self.threshold = threshold  # 密度峰值阈值
         pass
 
@@ -16,8 +23,33 @@ class CFSFDP:
     def get_distance(self):
         pass
 
-    def compute_distance(self):
-        pass
+    def compute_distance(self, x: np.ndarray[float], y: np.ndarray[float], distance_pattern: str = "euclidean_distance") -> float:
+        """
+        计算两个数据点之间的距离
+
+        :param distance_pattern: 距离类型。默认欧氏距离
+        :param x: x 点的坐标
+        :param y: y 点的坐标
+        :return: x 和 y 之间的距离
+        """
+
+        # 检查两点的坐标列表长度是否相等
+        if x.size != y.size:
+            raise Exception("两点坐标列表长度不同")
+        # 检查距离类型输入
+        if distance_pattern not in self.pattern:
+            raise Exception("不存在这种距离计算模式")
+
+        # 根据 pattern 使用对应的函数
+
+    def compute_euclidean_distance(self, x: np.ndarray[float], y: np.ndarray[float]) -> float:
+        """
+        计算两个数据点之间的欧式距离
+
+        :param x: x 点的坐标
+        :param y: y 点的坐标
+        :return: x 和 y 之间的欧氏距离
+        """
 
     def build_distance_matrix(self):
         pass
