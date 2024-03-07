48be5fed23548149ec07a869957a2b6ac89c683c
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-03-07 15:15:02 +0800

    Update load_data.py

--- emotion_vectors/preprocess/load_data.py
+++ emotion_vectors/preprocess/load_data.py
@@ -2,7 +2,7 @@ import pandas
 
 
 def load_emotion_dictionary():
-    df = pandas.read_csv("../情感词.csv", usecols=[0, 1, 2, 3, 4, 5, 6])
+    df = pandas.read_csv("../情感词.csv", usecols=[0, 1, 4, 5])
 
     return df
 
