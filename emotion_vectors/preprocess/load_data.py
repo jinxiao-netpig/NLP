0e553d00ec53d4a92b37eae790522e234516fe39
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-03-07 14:54:52 +0800

    add the method of load emotion dictionary

--- emotion_vectors/preprocess/load_data.py
+++ emotion_vectors/preprocess/load_data.py
@@ -1,2 +1,11 @@
+import pandas
+
+
 def load_emotion_dictionary():
-    pass
+    df = pandas.read_csv("../情感词.csv", usecols=[0, 1, 2, 3, 4, 5, 6])
+
+    return df
+
+
+if __name__ == '__main__':
+    print(load_emotion_dictionary())
