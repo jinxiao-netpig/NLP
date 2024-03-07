f13ceb49c01d88c69bf9833f517aa2ab5a194a1a
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-03-07 15:30:48 +0800

    Update load_data.py

--- emotion_vectors/preprocess/load_data.py
+++ emotion_vectors/preprocess/load_data.py
@@ -1,8 +1,16 @@
-import pandas
+import pandas as pd
 
 
-def load_emotion_dictionary():
-    df = pandas.read_csv("../情感词.csv", usecols=[0, 1, 4, 5])
+def load_emotion_dictionary() -> pd.DataFrame:
+    """
+    加载情感字典
+
+    词语  词性种类    情感分类    强度
+
+    :return: 情感词汇集合
+    """
+
+    df = pd.read_csv("../情感词.csv", usecols=[0, 1, 4, 5])
 
     return df
 
