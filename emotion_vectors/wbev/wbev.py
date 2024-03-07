5cb01a7895dc0f528654e93dd3f01bd2d7378842
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-03-07 16:49:12 +0800

    Update wbev.py

--- emotion_vectors/wbev/wbev.py
+++ emotion_vectors/wbev/wbev.py
@@ -5,13 +5,17 @@ import pandas as pd
 jieba.enable_paddle()
 
 emotion_list = {
-    "NN": 0
+    "NN": 0,
+    "NE": 1,
+    "ND": 2,
+    "NB":
 }
 
 
 class WBEV:
     def __init__(self, emotion_dictionary: pd.DataFrame):
         self.emotion_dictionary = emotion_dictionary
+        emotion_list = {}
 
     def __get_emotion_vector(self, title: str) -> np.ndarray:
         title_list = np.zeros(21, dtype=float)
@@ -26,3 +30,10 @@ class WBEV:
 
     def get_emotion_vector(self, title: str) -> np.ndarray:
         return self.__get_emotion_vector(title)
+
+    def __build_emotion_list(self):
+        emotions = self.emotion_dictionary["情感分类"]
+        index = 1
+
+        for word in emotions:
+            if word not in
