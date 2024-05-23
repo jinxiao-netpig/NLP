155a3fe3fc5fc47acd5e1616f5543e89a03ba3fe
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-05-23 17:50:10 +0800

    modify MetaMethod

--- English-Keyphrase-Extraction-master/model/ake/meta_method.py
+++ English-Keyphrase-Extraction-master/model/ake/meta_method.py
@@ -1,19 +1,31 @@
+import logging
 from abc import abstractmethod
 
 
 class MetaMethod:
     output_list = {}  # 原始关键词:预测关键词
+    stopwords = set()  # 停用词列表
 
     @abstractmethod
     def __init__(self):
         pass
 
     def load_stopwords(self):
+        logging.info("MODEL: " + str(type(self)) + ", " + "OPERATION: " + "load_stopwords")
         pass
 
     def filter_documents(self):
+        logging.info("MODEL: " + str(type(self)) + ", " + "OPERATION: " + "filter_documents")
         pass
 
     @abstractmethod
     def keyword_extraction(self):
+        pass
+
+    @abstractmethod
+    def computer_metric(self):
+        pass
+
+    @abstractmethod
+    def download_data(self):
         pass
--- English-Keyphrase-Extraction-master/model/ake/text_rank.py
+++ English-Keyphrase-Extraction-master/model/ake/text_rank.py
@@ -1,14 +1,17 @@
-class TEXTRANK:
-    output_list = {}  # 原始关键词:预测关键词
+import kex
 
+from model.ake.meta_method import MetaMethod
+
+
+class TEXTRANK(MetaMethod):
     def __init__(self):
-        pass
-
-    def load_stopwords(self):
-        pass
-
-    def filter_documents(self):
         pass
 
     def keyword_extraction(self):
         pass
+
+
+if __name__ == '__main__':
+    json_line, language = kex.get_benchmark_dataset('Inspec')
+    print("json_line: ", json_line[0], len(json_line), type(json_line))
+    print("language: ", language)
