c2f8a8a5bef31bec358c405510479e4d05ff2c6c
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-05-23 22:53:55 +0800

    Create topic_rank.py

---
+++ English-Keyphrase-Extraction-master/model/ake/topic_rank.py
@@ -0,0 +1,45 @@
+import time
+
+import kex
+
+from model.ake.meta_method import MetaMethod
+
+
+class TopicRank(MetaMethod):
+    def __init__(self):
+        pass
+
+    def keyword_extraction(self, dataset_name: str):
+        time1 = time.time()
+        self.load_stopwords()
+        self.filter_documents()
+
+        model = kex.TopicRank()
+        # 测试集取前500条数据
+        size = 500
+        if size > len(kex.get_benchmark_dataset(dataset_name)):
+            size = len(kex.get_benchmark_dataset(dataset_name))
+        json_line, _ = kex.get_benchmark_dataset(dataset_name)[:size]
+        for line in json_line:
+            text = line['source']
+            results = model.get_keywords(text, n_keywords=3)
+            predict_keywords = []
+            # 构建结果
+            for result in results:
+                predict_keywords.append(result['stemmed'])
+            self.output_list[";".join(line['keywords'])] = predict_keywords
+
+        time2 = time.time()
+        self.cost = int(time2 - time1)
+
+    def computer_metric(self):
+        pass
+
+    def download_data(self):
+        pass
+
+
+if __name__ == '__main__':
+    topic_rank_model = TopicRank()
+    topic_rank_model.keyword_extraction("Inspec")
+    topic_rank_model.show_output_list()
