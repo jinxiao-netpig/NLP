bf6367ad38665e985b5e252fb02acd22a04df305
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-05-28 17:12:51 +0800

    add compute_metric in meta_method

--- English-Keyphrase-Extraction-master/model/ake/meta_method.py
+++ English-Keyphrase-Extraction-master/model/ake/meta_method.py
@@ -3,11 +3,18 @@ from abc import abstractmethod
 
 
 class MetaMethod:
-    output_list = {}  # 原始关键词:预测关键词    str:str
+    output_list: dict[str, list[str]] = {}  # 原始关键词:预测关键词    str:list[str]
     stopwords = set()  # 停用词列表
     test_documents = []  # 预处理后的测试集文档列表
     train_documents = []  # 预处理后的训练集文档列表
     cost: int  # 提取关键词时间花费
+    tp: int = 0
+    fp: int = 0
+    tn: int = 0
+    fn: int = 0
+    precision: float
+    recall: float
+    f_score: float
 
     @abstractmethod
     def __init__(self):
@@ -27,9 +34,27 @@ class MetaMethod:
         self.filter_documents(dataset_name)
         pass
 
-    @abstractmethod
     def compute_metric(self):
-        pass
+        """
+        计算 P、R、F1
+
+        :return:
+        """
+
+        tp_fn_sum = 0
+        for oris, preds in self.output_list.items():
+            for pred in preds:
+                if pred in oris:
+                    self.tp += 1
+                else:
+                    self.fp += 1
+            oris_list = oris.split(sep=";")
+            tp_fn_sum += len(oris_list)
+        self.fn = tp_fn_sum - self.tp
+
+        self.precision = self.tp / (self.tp + self.fp)
+        self.recall = self.tp / (self.tp + self.fn)
+        self.f_score = 2 * self.precision * self.recall / (self.recall + self.precision)
 
     def train_model(self):
         # 目前训练出来的模型参数写死在函数体
