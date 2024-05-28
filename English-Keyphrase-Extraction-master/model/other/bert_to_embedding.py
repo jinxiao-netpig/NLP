599f0b96e540137dc426c6724135462894ad284f
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-05-28 14:47:38 +0800

    Update bert_to_embedding.py

--- English-Keyphrase-Extraction-master/model/other/bert_to_embedding.py
+++ English-Keyphrase-Extraction-master/model/other/bert_to_embedding.py
@@ -34,8 +34,13 @@ class BertToEmbedding(ToEmbedding):
 
         token_embeddings = []
         for token_i in range(len(tokenized_text)):
+            print("token_i: {}".format(token_i))
+            print("len(tokenized_text): {}".format(len(tokenized_text)))
             hidden_layers = []
             for layer_i in range(len(encoded_layers)):
+                print("layer_i: {}".format(layer_i))
+                print("len(encoded_layers[layer_i]): {}".format(len(encoded_layers[layer_i])))
+                print("len(encoded_layers[layer_i][0]): {}".format(len(encoded_layers[layer_i][0])))
                 vec = encoded_layers[layer_i][0][token_i]
                 hidden_layers.append(vec)
             token_embeddings.append(hidden_layers)
@@ -46,7 +51,26 @@ class BertToEmbedding(ToEmbedding):
 
 
 if __name__ == '__main__':
-    text = "I want to eat a apple."
-    bert_model = BertToEmbedding()
-    bert_model.text_to_embedding(text=text)
-    print(bert_model.get_embedding())
+    # text = "I want to eat a apple."
+    # bert_model = BertToEmbedding()
+    # bert_model.text_to_embedding(text=text)
+    # print(bert_model.get_embedding())
+    text = " the man went to the store "
+    tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
+    model = BertModel.from_pretrained("bert-base-cased")
+    tokenized_text = tokenizer.tokenize(text)  # token初始化
+    indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)  # 获取词汇表索引
+    tokens_tensor = torch.tensor([indexed_tokens])  # 将输入转化为torch的tensor
+    with torch.no_grad():  # 禁用梯度计算 因为只是前向传播获取隐藏层状态，所以不需要计算梯度
+        last_hidden_states = model(tokens_tensor)[0]
+    token_embeddings = []
+    for token_i in range(len(tokenized_text)):
+        hidden_layers = []
+        for layer_i in range(len(last_hidden_states)):
+            vec = last_hidden_states[layer_i][0][token_i]  # 如果输入是单句不分块中间是0，因为只有一个维度，如果分块还要再遍历一次
+            hidden_layers.append(vec)
+        token_embeddings.append(hidden_layers)
+
+    sentence_embedding = torch.mean(last_hidden_states[11], 1).numpy()
+    print(sentence_embedding)
+
