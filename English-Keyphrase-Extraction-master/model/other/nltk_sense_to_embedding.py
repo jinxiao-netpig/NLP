3b8566c972ea1a1568f86504c48447bf9e922410
JinXiao <110443900+1793925850@users.noreply.github.com>
2024-05-29 20:23:04 +0800

    Create nltk_sense_to_embedding.py

---
+++ English-Keyphrase-Extraction-master/model/other/nltk_sense_to_embedding.py
@@ -0,0 +1,9 @@
+from model.other.to_embedding import ToEmbedding
+
+
+class NltkSenseToEmbedding(ToEmbedding):
+    def __init__(self):
+        pass
+
+    def text_to_embedding(self, text: str):
+        pass
